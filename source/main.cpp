6fe12a68f3309dbdaad3dc4efe72e3745f81b0b8
Your Name <you@example.com>
2025-04-01 00:00:46 +0100

    notify only

--- source/main.cpp
+++ source/main.cpp
@@ -18,7 +18,9 @@ extern "C" {
 #include <ps5/kernel.h>
 }
 
-#define ENABLE_LOGS 1
+
+
+#define ENABLE_LOGS 0
 #define PC_IP   "192.168.1.3"
 #define PC_PORT 5655
 
@@ -28,6 +30,15 @@ extern "C" {
 #define NAND_A53IO_DISABLE_CONTROLLER   0x80046104
 
 #define PUP_UPDATER_READ_NAND_GROUP     0xC018440A
+
+typedef struct notify_request {
+  char useless1[45];
+  char message[3075];
+} notify_request_t;
+
+extern "C" {
+	int sceKernelSendNotificationRequest(int, notify_request_t*, size_t, int);
+}
 
 struct ioctl_readnandgroup_args
 {
@@ -40,7 +51,7 @@ int g_debug_sock;
 
 int sock;
 
-void sock_print(int sock, char *str)
+/*void sock_print(int sock, char *str)
 {
 	size_t size = 0;
 
@@ -48,7 +59,14 @@ void sock_print(int sock, char *str)
 	size = strlen(str);
 	write(sock, str, size);
 #endif
-}
+}*/
+
+#define printf_notification(fmt, ...) \
+{   notify_request_t req; \
+	bzero(&req, sizeof req); \
+	snprintf(req.message, sizeof req.message, fmt, ##__VA_ARGS__); \
+	sceKernelSendNotificationRequest(0, &req, sizeof req, 0); \
+} while(0);
 
 int main(){
 	
@@ -62,14 +80,14 @@ int main(){
     kernel_set_proc_rootdir(getpid(), kernel_get_root_vnode());
 	
 	int ret;
-    int sock;
+    //int sock;
     int out_fds[3];
     int a53_fd;
     int pupupdate_fd;
     int zero;
     int written_bytes;
-    char printbuf[128];
-    struct sockaddr_in addr;
+    //char printbuf[128];
+    //struct sockaddr_in addr;
     void *out_data;
     uint64_t nand_size;
     
@@ -80,6 +98,7 @@ int main(){
     pupupdate_fd = -1;
     out_data     = NULL;
 	
+	/*
 	sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         return -1;
@@ -94,67 +113,67 @@ int main(){
     if (ret < 0) {
         return -1;
     }
-	
+	*/
 	mkdir("/data/PS5", 0777);
 
     out_fds[0] = open("/data/PS5/nandgroup0.bin", O_WRONLY | O_CREAT, 0644);
     if (out_fds[0] < 0) {
-        sprintf(printbuf, "[+] failed to open output file (%d)\n", errno);
-        sock_print(sock, printbuf);
-        close(sock);
+        printf_notification("[+] failed to open output file (%d)\n", errno);
+        //sock_print(sock, printbuf);
+        //close(sock);
         return -1;
     }
 
     out_fds[1] = open("/data/PS5/nandgroup1.bin", O_WRONLY | O_CREAT, 0644);
     if (out_fds[0] < 0) {
-        sprintf(printbuf, "[+] failed to open output file (%d)\n", errno);
-        sock_print(sock, printbuf);
-        close(sock);
+        printf_notification("[+] failed to open output file (%d)\n", errno);
+        //sock_print(sock, printbuf);
+        //close(sock);
         return -1;
     }
 
     out_fds[2] = open("/data/PS5/nandgroup2.bin", O_WRONLY | O_CREAT, 0644);
     if (out_fds[0] < 0) {
-        sprintf(printbuf, "[+] failed to open output file (%d)\n", errno);
-        sock_print(sock, printbuf);
-        close(sock);
+        printf_notification("[+] failed to open output file (%d)\n", errno);
+        //sock_print(sock, printbuf);
+        //close(sock);
         return -1;
     }
 	
 	// Open A53IO device to configure NAND
     a53_fd = open("/dev/a53io", 2, 0);
-    sprintf(printbuf, "[+] a53io dev = 0x%x (errno = %d)\n", a53_fd, errno);
-    sock_print(sock, printbuf);
+    printf_notification("[+] a53io dev = 0x%x (errno = %d)\n", a53_fd, errno);
+    //sock_print(sock, printbuf);
 	
 	if (a53_fd < 0) {
-        sprintf(printbuf, "[!] failed to open a53 :(\n");
-        sock_print(sock, printbuf);
+        printf_notification("[!] failed to open a53 :(\n");
+        //sock_print(sock, printbuf);
         goto out;
     }
 	
 	// Disable the A53 controller, which is necessary to expose NAND to read
     ret = ioctl(a53_fd, NAND_A53IO_DISABLE_CONTROLLER, &zero);
-    sprintf(printbuf, "[+] disable controller = 0x%x (errno = %d)\n", ret, errno);
-    sock_print(sock, printbuf);
+    printf_notification("[+] disable controller = 0x%x (errno = %d)\n", ret, errno);
+    //sock_print(sock, printbuf);
 	
 	// Map buffer for output NAND group data
     out_data = mmap(0, 0x4000000, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     if (out_data == NULL) {
-        sprintf(printbuf, "[!] failed to map memory for nand data (%d) :(\n", errno);
-        sock_print(sock, printbuf);
+        printf_notification("[!] failed to map memory for nand data (%d) :(\n", errno);
+        //sock_print(sock, printbuf);
         goto out;
     }
 
     // Open pup update FD to do reading
     pupupdate_fd = open("/dev/pup_update0", 2, 0);
     if (pupupdate_fd < 0) {
-        sprintf(printbuf, "[!] failed to open pup_update0 (%d) :(\n",  errno);
-        sock_print(sock, printbuf);
+        printf_notification("[!] failed to open pup_update0 (%d) :(\n",  errno);
+        //sock_print(sock, printbuf);
         goto out;
     }
 
-    sprintf(printbuf, "[+] pup update dev = 0x%x (errno = %d)\n", pupupdate_fd, errno);
-    sock_print(sock, printbuf);
+    printf_notification("[+] pup update dev = 0x%x (errno = %d)\n", pupupdate_fd, errno);
+    //sock_print(sock, printbuf);
 	
 	// Max 3 NAND groups
     for (int i = 0; i < 3; i++) {
@@ -181,28 +200,28 @@ int main(){
         // Read NAND data
         ret = ioctl(pupupdate_fd, PUP_UPDATER_READ_NAND_GROUP, &ioc_args);
         if (ret != 0) {
-            sprintf(printbuf, "[!] failed to read NAND (%d) :(\n", errno);
-            sock_print(sock, printbuf);
+            printf_notification("[!] failed to read NAND (%d) :(\n", errno);
+            //sock_print(sock, printbuf);
             goto out;
         }
 
-        sprintf(printbuf, "[+] read nand group %d = %d (errno = %d)\n", i, ret, errno);
-        sock_print(sock, printbuf);
+        printf_notification("[+] read nand group %d = %d (errno = %d)\n", i, ret, errno);
+        //sock_print(sock, printbuf);
 
         // Dump to file
         written_bytes = write(out_fds[i], out_data, nand_size);
         if (written_bytes != nand_size) {
-            sprintf(printbuf, "[!] failed to write nand to file, %d != %lu.\n", written_bytes, nand_size);
-            sock_print(sock, printbuf);
+            printf_notification("[!] failed to write nand to file, %d != %lu.\n", written_bytes, nand_size);
+            //sock_print(sock, printbuf);
             goto out;
         }
 
-        sprintf(printbuf, "[+] wrote %d bytes...\n", written_bytes);
-        sock_print(sock, printbuf);
+        printf_notification("[+] wrote %d bytes...\n", written_bytes);
+        //sock_print(sock, printbuf);
     }
 
-    sprintf(printbuf, "Done!\n");
-    sock_print(sock, printbuf);
+    printf_notification("Done!\n");
+    //sock_print(sock, printbuf);
 	
 out:
     if (out_data != NULL)
@@ -218,7 +237,7 @@ out:
     close(out_fds[1]);
     close(out_fds[2]);
 #if ENABLE_LOGS
-    close(sock);
+    //close(sock);
 #endif
   
 	return 0;
